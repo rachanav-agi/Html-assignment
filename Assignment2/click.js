96eaaecf3c7025ffdd14fd6d43939cfe036e0b8d
rachanav-agi <93185636+rachanav-agi@users.noreply.github.com>
2021-11-21 19:30:21 +0530

    Add files via upload

---
+++ Assignment2/click.js
@@ -0,0 +1,8 @@
+var btn=document.getElementById("btn");
+var box = document.getElementById("box");
+
+btn.addEventListener("click",function()
+{   
+    
+    box.style.backgroundColor = "blue";
+});
